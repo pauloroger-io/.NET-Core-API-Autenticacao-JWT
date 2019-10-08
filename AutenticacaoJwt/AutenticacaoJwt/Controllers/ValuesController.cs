640be927ebdadb354ce41194825ebdec47110fb0
paulorgn <paulorgndev@gmail.com>
2019-10-08 14:39:02 -0300

    Clear

--- AutenticacaoJwt/AutenticacaoJwt/Controllers/ValuesController.cs
+++ AutenticacaoJwt/AutenticacaoJwt/Controllers/ValuesController.cs
@@ -10,51 +10,25 @@ namespace AutenticacaoJwt.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
         [HttpGet]
         public ActionResult<string> Get()
         {
             string texto =
 
-@"Envie um POST para a rota => api/usuario/obtertoken
-com o JSON abaixo no body
+            @"Envie um POST para a rota => api/usuario/obtertoken
+            com o JSON abaixo no body
 
-{
- 'usuario': 'fd98fda89fda79f8da798ffds7fdsh79da',
- 'senha': 'fd98fda89fda79f8da798ffds7fdsh79daSenha',
- 'email': 'fd98fda89fda79f8da798ffds7fdsh79da@gmail.com'
-}
+            {
+             'usuario': 'fd98fda89fda79f8da798ffds7fdsh79da',
+             'senha': 'fd98fda89fda79f8da798ffds7fdsh79daSenha',
+             'email': 'fd98fda89fda79f8da798ffds7fdsh79da@gmail.com'
+            }
 
 
-Após receber o token, envie um GET para a rota api/usuario/testandotoken com o token como Authorization, TYPE Bearer Token
-";
+            Após receber o token, envie um GET para a rota api/usuario/testandotoken com o token como Authorization, TYPE Bearer Token
+            ";
 
             return texto;
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
